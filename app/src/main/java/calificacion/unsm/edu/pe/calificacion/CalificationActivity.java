bf0546f1f39ee69d89595028e76bf81d94c5c014
brapastor <brapastor@gmail.com>
2014-12-09 10:31:49 -0500

    Agregado relacion bd

--- app/src/main/java/calificacion/unsm/edu/pe/calificacion/CalificationActivity.java
+++ app/src/main/java/calificacion/unsm/edu/pe/calificacion/CalificationActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,17 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import net.HttpDispachet;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import adapter.GridEscuelasAdapter;
@@ -20,8 +32,9 @@ import model.ItemGrid;
 
 public class CalificationActivity extends Activity {
     private GridView gridView;
-    private static final int RESULT_CONFIG=1;
+    private static final int RESULT_CONFIG = 1;
     private ArrayList<ItemGrid> items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +45,7 @@ public class CalificationActivity extends Activity {
         //<editor-fold desc="CREATE GRID">
         items = new ArrayList<ItemGrid>();
         items.add(new ItemGrid("Sistemas e Informática", R.drawable.fisi));
-      //items.add(new ItemGrid("Ciencias Economicas", R.drawable.fce));
+        //items.add(new ItemGrid("Ciencias Economicas", R.drawable.fce));
         items.add(new ItemGrid("Agronomía", R.drawable.fca));
         items.add(new ItemGrid("Veterinaria", R.drawable.unsm_escudo));
         items.add(new ItemGrid("Enfermeria", R.drawable.enfermeria));
@@ -55,12 +68,12 @@ public class CalificationActivity extends Activity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), position+"", Toast.LENGTH_SHORT).show();
-                Intent intent=null;
-                switch (position){
+                Toast.makeText(getApplicationContext(), position + "", Toast.LENGTH_SHORT).show();
+                Intent intent = null;
+                switch (position) {
 
-                    case 0 :
-                        intent = new Intent(CalificationActivity.this,CriterionOne.class);
+                    case 0:
+                        intent = new Intent(CalificationActivity.this, CriterionOne.class);
                         break;
 
                 }
@@ -91,9 +104,10 @@ public class CalificationActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode){
+        switch (requestCode) {
             case RESULT_CONFIG:
                 showSettings();
+                createJurado();
                 break;
 
             default:
@@ -115,5 +129,80 @@ public class CalificationActivity extends Activity {
         SharedPreferences.Editor editor = mSharePreferences.edit();
         editor.putString("url", url);
         editor.commit();
+    }
+
+    private void createJurado() {
+        SharedPreferences mSharePreferences = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
+        String fullname = mSharePreferences.getString("preJurado", "NULL");
+        String profesion = mSharePreferences.getString("preProfesionJurado", "NULL");
+
+        RequestParams params = new RequestParams();
+        params.put("fullname", fullname);
+        params.put("profesion", profesion);
+        Log.i("url",getUrl()+"jurados/nuevo");
+
+        HttpDispachet.post(getUrl() + "jurados/nuevo", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                try {
+                    String value = new String(bytes, "UTF-8");
+                    JSONObject mObject = new JSONObject(value);
+                    JSONArray mArray = mObject.getJSONArray("datos");
+                    Log.e("OK",mObject.getString("Result"));
+                    if(mObject.getString("Result").equals("OK")){
+                        showJurado(mArray.getJSONObject(0).getInt("id"),
+                                mArray.getJSONObject(0).getString("profesion"),
+                                mArray.getJSONObject(0).getString("fullname"));
+                    }
+                    else{
+                        Log.e("error","Error");
+                    }
+                    //TODO: borrar
+                    Log.i("response", mArray.toString());
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Log.i("status", i + "");
+            }
+        });
+
+    }
+
+    public void showJurado(int id,String fullname,String profesion)
+    {
+        SharedPreferences mSharePreferences = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
+        mSharePreferences = getSharedPreferences("CalificationPreferences",
+                MODE_PRIVATE);
+
+        // guardamos las preferencias en un xml
+        SharedPreferences.Editor editor = mSharePreferences.edit();
+        editor.putString("fullname", fullname);
+        editor.putString("profesion", profesion);
+        editor.putInt("id", id);
+        editor.commit();
+        //TODO
+        Log.i("idJurado",getIdJurado()+"");
+    }
+
+    public String getUrl() {
+        SharedPreferences pref = getSharedPreferences(
+                "CalificationPreferences",MODE_PRIVATE);
+
+        return pref.getString("url", "nada");
+
+    }
+    public int getIdJurado() {
+        SharedPreferences pref = getSharedPreferences(
+                "CalificationPreferences",MODE_PRIVATE);
+        return pref.getInt("id", 0);
     }
 }
---
+++ app/src/main/java/net/HttpDispachet.java
@@ -0,0 +1,23 @@
+package net;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by BRAPASTOR on 06/12/2014.
+ */
+public class HttpDispachet  {
+
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+}
